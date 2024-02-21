f6d1769e34869280e3a853d664727ee7b52f15ae
Trent Meyer <tmeyer900@gmail.com>
2024-02-20 21:46:56 -0500

    define CORS settings to allow API access from websites

--- src/main/kotlin/dev/trentmyr/plugins/HTTP.kt
+++ src/main/kotlin/dev/trentmyr/plugins/HTTP.kt
@@ -7,11 +7,9 @@ import io.ktor.server.plugins.defaultheaders.*
 
 fun Application.configureHTTP() {
     install(CORS) {
-        allowMethod(HttpMethod.Options)
-        allowMethod(HttpMethod.Put)
-        allowMethod(HttpMethod.Delete)
-        allowMethod(HttpMethod.Patch)
-        allowHeader(HttpHeaders.Authorization)
+        allowMethod(HttpMethod.Post)
+        allowHeader(HttpHeaders.ContentType)
+//        allowHost("finaltouchcincy")
         anyHost() // @TODO: Don't do this in production if possible. Try to limit it.
     }
     install(DefaultHeaders) {
