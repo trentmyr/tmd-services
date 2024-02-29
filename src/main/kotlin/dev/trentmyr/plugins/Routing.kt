00a64b83b428d1bed566f22d9119dfbccc8545d4
Trent Meyer <tmeyer900@gmail.com>
2024-02-28 20:51:04 -0500

    respond with 200 status code to options request at final touch send quote

--- src/main/kotlin/dev/trentmyr/plugins/Routing.kt
+++ src/main/kotlin/dev/trentmyr/plugins/Routing.kt
@@ -2,6 +2,7 @@ package dev.trentmyr.plugins
 
 import dev.trentmyr.client.SendGridClient
 import dev.trentmyr.models.sendgrid.templateData.QuoteRequest
+import io.ktor.http.*
 import io.ktor.server.application.*
 import io.ktor.server.response.*
 import io.ktor.server.routing.*
@@ -17,10 +18,15 @@ fun Application.configureRouting() {
 fun Routing.finalTouchRoutes() {
     // TODO: require auth key from client in order to accept request
     route("/finalTouch") {
-        post<QuoteRequest>("/sendQuoteRequest") { quoteRequest ->
-            LOGGER.info("Processing incoming quote request with data: $quoteRequest")
-            val response = SendGridClient.sendRequestForQuote(quoteRequest)
-            call.respond(response.status)
+        route("/sendQuoteRequest") {
+            options {
+                call.respond(HttpStatusCode.OK)
+            }
+            post<QuoteRequest> { quoteRequest ->
+//                LOGGER.info("Processing incoming quote request with data: $quoteRequest")
+                val response = SendGridClient.sendRequestForQuote(quoteRequest)
+                call.respond(response.status)
+            }
         }
     }
 }
